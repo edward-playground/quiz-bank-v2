1e08764c75b372027453bbeb92dea93e4aa934c0
edwlee <edwlee@student.ethz.ch>
2026-02-20 21:13:46 -0800

    Initial update

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // 確保 production assets 從 /assets/... 讀取
-})
